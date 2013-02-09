0476c7236815a0a26b90ddfc93852c9d603b1db0
Ryan Keavney <keavneyrj1@gcc.edu>
2013-02-09 00:27:31 -0500

    altered start-up project properties

--- treads/treads/AppDelegate.m
+++ treads/treads/AppDelegate.m
@@ -8,11 +8,19 @@
 
 #import "AppDelegate.h"
 
+#include "MainViewController.h"
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    
+    self.window.rootViewController = [[MainViewController alloc] init];
+    
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 							
--- treads/treads/MainViewController.h
+++ treads/treads/MainViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MainViewController.h
 //  treads
 //
 //  Created by keavneyrj1 on 2/9/13.
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface MainViewController : UIViewController
 
 @end
