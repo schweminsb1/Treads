ddc5a3f8f7f6cee7531f68e563a8da752a7908df
DeLeoneAM1 <DeLeoneAM1@gcc.edu>
2013-03-08 18:52:39 -0500

    Added AddLocation View Controller

---
+++ treads/treads/AddLocationViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddLocationViewController.h
+//  treads
+//
+//  Created by Anthony DeLeone on 3/8/13.
+//  Copyright (c) 2013 Team Walking Stick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddLocationViewController : UIViewController
+
+@end
---
+++ treads/treads/AddLocationViewController.m
@@ -0,0 +1,38 @@
+//
+//  AddLocationViewController.m
+//  treads
+//
+//  Created by Anthony DeLeone on 3/8/13.
+//  Copyright (c) 2013 Team Walking Stick. All rights reserved.
+//
+
+#import "AddLocationViewController.h"
+
+@interface AddLocationViewController ()
+
+@end
+
+@implementation AddLocationViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
