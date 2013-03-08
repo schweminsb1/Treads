2a753d8e23c745efaf07463729f74b1a3233722f
DeLeoneAM1 <DeLeoneAM1@gcc.edu>
2013-03-08 16:56:38 -0500

    Added Trip View View Controller and CoreLocation Framework for map

---
+++ treads/treads/TripViewVC.h
@@ -0,0 +1,13 @@
+//
+//  TripViewVC.h
+//  treads
+//
+//  Created by Anthony DeLeone on 3/8/13.
+//  Copyright (c) 2013 Team Walking Stick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TripViewVC : UIViewController
+
+@end
---
+++ treads/treads/TripViewVC.m
@@ -0,0 +1,38 @@
+//
+//  TripViewVC.m
+//  treads
+//
+//  Created by Anthony DeLeone on 3/8/13.
+//  Copyright (c) 2013 Team Walking Stick. All rights reserved.
+//
+
+#import "TripViewVC.h"
+
+@interface TripViewVC ()
+
+@end
+
+@implementation TripViewVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
