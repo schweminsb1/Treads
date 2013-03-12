2a09c34b5366609df3508cfff1017669f432b262
sam schwemin <schweminsb1@gcc.edu>
2013-03-12 19:46:17 -0400

    treadssession works

---
+++ treads/treads/TreadsSession.h
@@ -0,0 +1,22 @@
+//
+//  TreadsSession.h
+//  treads
+//
+//  Created by Sam Schwemin on 3/7/13.
+//  Copyright (c) 2013 Team Walking Stick. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TreadsSession : NSObject
+
+
+@property NSString * treadsUser;
+
+-(id) initWithAuthenticatedUser: (NSString *)user;
+-(BOOL) Login;
+-(BOOL) Logout;
+-(NSString * ) valueOfFile;
+
+
+@end
---
+++ treads/treads/TreadsSession.m
@@ -0,0 +1,70 @@
+//
+//  TreadsSession.m
+//  treads
+//
+//  Created by Sam Schwemin on 3/7/13.
+//  Copyright (c) 2013 Team Walking Stick. All rights reserved.
+//
+
+#import "TreadsSession.h"
+
+@implementation TreadsSession
+
+-(id) initWithAuthenticatedUser: (NSString *)user
+{
+    if(self= [super init])
+    {
+        _treadsUser=user;
+        
+    }
+    return self;
+}
+-(BOOL) Login
+{
+     __autoreleasing NSError * error;
+    @try
+    {
+       
+        NSString * test = [Path subLibraryCachesDirectory:@"User"];
+        [ _treadsUser writeToFile:test  atomically:YES encoding:NSUTF8StringEncoding error:&error];
+    }
+    @catch (id exception)
+    {
+        NSLog(@"%@",error );
+        return NO;
+    }
+    
+    
+    return YES;
+}
+-(BOOL) Logout
+{
+     __autoreleasing NSError * error;
+    @try
+    {
+        
+        NSString * test = [Path subLibraryCachesDirectory:@"User"];
+        [ @"" writeToFile:test  atomically:YES encoding:NSUTF8StringEncoding error:&error];
+    }
+    @catch (id exception)
+    {
+        NSLog(@"%@",error );
+        return NO;
+    }
+    
+    return YES;
+}
+-(NSString * ) valueOfFile
+{
+     NSString * filePath = [Path subLibraryCachesDirectory:@"User"];
+     NSString * fileString = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
+    
+    
+    return fileString;
+}
+
+
+
+
+
+@end
