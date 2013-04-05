27981c99d8867c422c1e00b6f757fa8b3c575c2a
sam schwemin <schweminsb1@gcc.edu>
2013-04-05 00:44:55 -0400

    blob framework added

--- treads/treads/DataRepository.h
+++ treads/treads/DataRepository.h
@@ -6,19 +6,36 @@
 //  Copyright (c) 2013 Team Walking Stick. All rights reserved.
 //
 
+
+//http://chrisrisner.com/iOS-and-Mobile-Services-and-Windows-Azure-Storage
+
 #import <Foundation/Foundation.h>
 #import <WindowsAzureMobileServices/WindowsAzureMobileServices.h>
 #import "TreadsService.h"
 
 @interface DataRepository : NSObject
 
-typedef void (^ CompletionWithSasBlock)(NSURL*);
+typedef void (^CompletionBlock) ();
+typedef void (^CompletionWithIndexBlock) (NSUInteger index);
+typedef void (^CompletionWithMessagesBlock) (id messages);
+typedef void (^CompletionWithSasBlock) (NSString *sasUrl);
+typedef void (^BusyUpdateBlock) (BOOL busy);
 
 @property MSClient* client;
 @property MSTable * tablesTable;
 @property MSTable * tableRowsTable;
 @property MSTable * containersTable;
 @property MSTable * blobsTable;
+
+@property (nonatomic, strong)   NSArray *tables;
+@property (nonatomic, strong)   NSArray *tableRows;
+@property (nonatomic, strong)   NSArray *containers;
+@property (nonatomic, strong)   NSArray *blobs;
+
+
+@property (nonatomic)           NSInteger busyCount;
+@property (nonatomic, copy)     BusyUpdateBlock busyUpdate;
+
 
 @property NSURL * SasURL;
 //will be used to make requests to the server via this URL so do NSMutableURLREquest to get information hopefully from the Blob
@@ -38,6 +55,28 @@ typedef void (^ CompletionWithSasBlock)(NSURL*);
 - (void) getLocationsOrdered: (MSReadQueryBlock) getAll;
 
 -(void) getCommentsFromLocationID: (NSString *) locationID withBlock: (MSReadQueryBlock) getComments;
+
+
+//Blob storage helper functions
+- (void) handleRequest:(NSURLRequest *)request
+                onNext:(MSFilterNextBlock)onNext
+            onResponse:(MSFilterResponseBlock)onResponse;
+
+- (void) refreshTablesOnSuccess:(CompletionBlock) completion;
+- (void) refreshTableRowsOnSuccess:(NSString *)tableName withCompletion:(CompletionBlock) completion;
+- (void) updateTableRow:(NSDictionary *)item withTableName:(NSString *)tableName withCompletion:(CompletionBlock) completion;
+- (void) insertTableRow:(NSDictionary *)item withTableName:(NSString *)tableName withCompletion:(CompletionBlock) completion;
+- (void) createTable:(NSString *)tableName withCompletion:(CompletionBlock) completion;
+- (void) deleteTable:(NSString *)tableName withCompletion:(CompletionBlock) completion;
+- (void) deleteTableRow:(NSDictionary *)item withTableName:(NSString *)tableName withCompletion:(CompletionBlock) completion;
+
+- (void) refreshContainersOnSuccess:(CompletionBlock) completion;
+- (void) createContainer:(NSString *)containerName withPublicSetting:(BOOL)isPublic withCompletion:(CompletionBlock) completion;
+- (void) deleteContainer:(NSString *)containerName withCompletion:(CompletionBlock) completion;
+- (void) refreshBlobsOnSuccess:(NSString *)containerName withCompletion:(CompletionBlock) completion;
+- (void) deleteBlob:(NSString *)blobName fromContainer:(NSString *)containerName withCompletion:(CompletionBlock) completion;
+- (void) getSasUrlForNewBlob:(NSString *)blobName forContainer:(NSString *)containerName withCompletion:(CompletionWithSasBlock) completion;
+
 
 
 @end
--- treads/treads/DataRepository.m
+++ treads/treads/DataRepository.m
@@ -22,7 +22,9 @@
         self.containersTable = [_client getTable:@"BlobContainers"];
         self.blobsTable = [_client getTable:@"BlobBlobs"];
         
-        //self.tables = [[NSMutableArray alloc] init];
+        self.tables = [[NSMutableArray alloc] init];
+        
+        self.busyCount = 0;
     }
     return self;
 }
@@ -161,15 +163,249 @@
     
 }
 
-typedef void (^ CompletionWithSasBlock)(NSURL*);
+
+- (void) busy:(BOOL) busy
+{
+    // assumes always executes on UI thread
+    if (busy) {
+        if (self.busyCount == 0 && self.busyUpdate != nil) {
+            self.busyUpdate(YES);
+        }
+        self.busyCount ++;
+    }
+    else
+    {
+        if (self.busyCount == 1 && self.busyUpdate != nil) {
+            self.busyUpdate(FALSE);
+        }
+        self.busyCount--;
+    }
+}
+
+- (void) logErrorIfNotNil:(NSError *) error
+{
+    if (error) {
+        NSLog(@"ERROR %@", error);
+    }
+}
+////blob storage functions
+
+- (void) refreshTablesOnSuccess:(CompletionBlock) completion{
+    [self.tablesTable readWithCompletion:^(NSArray *results, NSInteger totalCount, NSError *error) {
+        
+        [self logErrorIfNotNil:error];
+        
+        self.tables = [results mutableCopy];
+        
+        // Let the caller know that we finished
+        completion();
+    }];
+}
+
+- (void) refreshTableRowsOnSuccess:(NSString *)tableName withCompletion:(CompletionBlock) completion {
+    
+    
+    NSString *queryString = [NSString stringWithFormat:@"table=%@", tableName];
+    
+    [self.tableRowsTable readWithQueryString:queryString completion:^(NSArray *results, NSInteger totalCount, NSError *error) {
+        
+        [self logErrorIfNotNil:error];
+        
+        self.tableRows = [results mutableCopy];
+        
+        // Let the caller know that we finished
+        completion();
+    }];
+}
+
+- (void) updateTableRow:(NSDictionary *)item withTableName:(NSString *)tableName withCompletion:(CompletionBlock) completion {
+    NSLog(@"Update Table Row %@", item);
+    
+    NSDictionary *params = @{ @"table" : tableName };
+    
+    [self.tableRowsTable update:item parameters:params completion:^(NSDictionary *result, NSError *error) {
+        
+        [self logErrorIfNotNil:error];
+        
+        NSLog(@"Results: %@", result);
+        
+        // Let the caller know that we finished
+        completion();
+    }];
+}
+
+- (void) insertTableRow:(NSDictionary *)item withTableName:(NSString *)tableName withCompletion:(CompletionBlock) completion {
+    NSLog(@"Insert Table Row %@", item);
+    
+    NSDictionary *params = @{ @"table" : tableName };
+    
+    [self.tableRowsTable insert:item parameters:params completion:^(NSDictionary *result, NSError *error) {
+        
+        [self logErrorIfNotNil:error];
+        
+        NSLog(@"Results: %@", result);
+        
+        // Let the caller know that we finished
+        completion();
+    }];
+}
+
+- (void) createTable:(NSString *)tableName withCompletion:(CompletionBlock) completion {
+    NSDictionary *item = @{ @"tableName" : tableName };
+    
+    [self.tablesTable insert:item completion:^(NSDictionary *result, NSError *error) {
+        
+        [self logErrorIfNotNil:error];
+        
+        NSLog(@"Results: %@", result);
+        
+        // Let the caller know that we finished
+        completion();
+    }];
+}
+
+- (void) deleteTable:(NSString *)tableName withCompletion:(CompletionBlock) completion {
+    NSDictionary *idItem = @{ @"id" :@0 };
+    NSDictionary *params = @{ @"tableName" : tableName };
+    
+    [self.tablesTable delete:idItem parameters:params completion:^(NSNumber *itemId, NSError *error) {
+        [self logErrorIfNotNil:error];
+        
+        NSLog(@"Results: %@", itemId);
+        
+        // Let the caller know that we finished
+        completion();
+    }];
+}
+
+- (void) deleteTableRow:(NSDictionary *)item withTableName:(NSString *)tableName withCompletion:(CompletionBlock) completion {
+    NSLog(@"Delete Table Row %@", item);
+    
+    //Have to send over all three as params since Mobile Services
+    //will strip everything but the ID from the item dictionary
+    NSDictionary *params = @{ @"tableName" : tableName ,
+                              @"partitionKey" : [item objectForKey:@"PartitionKey"] ,
+                              @"rowKey" : [item objectForKey:@"RowKey"]};
+    
+    [self.tableRowsTable delete:item parameters:params completion:^(NSNumber *itemId, NSError *error) {
+        
+        
+        [self logErrorIfNotNil:error];
+        
+        NSLog(@"Results: %@", itemId);
+        
+        // Let the caller know that we finished
+        completion();
+    }];
+}
+
+
+
+
+
+#pragma mark * MSFilter methods
+
+
+- (void) handleRequest:(NSURLRequest *)request
+                onNext:(MSFilterNextBlock)onNext
+            onResponse:(MSFilterResponseBlock)onResponse
+{
+    // A wrapped response block that decrements the busy counter
+    MSFilterResponseBlock wrappedResponse = ^(NSHTTPURLResponse *response, NSData *data, NSError *error) {
+        [self busy:NO];
+        onResponse(response, data, error);
+    };
+    
+    // Increment the busy counter before sending the request
+    [self busy:YES];
+    onNext(request, wrappedResponse);
+}
+
+
+
+
+#pragma blobs
+
+- (void) refreshContainersOnSuccess:(CompletionBlock) completion{
+    [self.containersTable readWithCompletion:^(NSArray *results, NSInteger totalCount, NSError *error) {
+        
+        [self logErrorIfNotNil:error];
+        
+        self.containers = [results mutableCopy];
+        
+        // Let the caller know that we finished
+        completion();
+    }];
+}
+
+- (void) createContainer:(NSString *)containerName withPublicSetting:(BOOL)isPublic withCompletion:(CompletionBlock) completion {
+    NSDictionary *item = @{ @"containerName" : containerName };
+    
+    NSDictionary *params = @{ @"isPublic" : [NSNumber numberWithBool:isPublic] };
+    
+    [self.containersTable insert:item parameters:params completion:^(NSDictionary *result, NSError *error) {
+        
+        [self logErrorIfNotNil:error];
+        
+        NSLog(@"Results: %@", result);
+        
+        // Let the caller know that we finished
+        completion();
+    }];
+}
+
+- (void) deleteContainer:(NSString *)containerName withCompletion:(CompletionBlock) completion {
+    NSDictionary *idItem = @{ @"id" :@0 };
+    NSDictionary *params = @{ @"containerName" : containerName };
+    
+    [self.containersTable delete:idItem parameters:params completion:^(NSNumber *itemId, NSError *error) {
+        [self logErrorIfNotNil:error];
+        
+        NSLog(@"Results: %@", itemId);
+        
+        // Let the caller know that we finished
+        completion();
+    }];
+}
+
+- (void) refreshBlobsOnSuccess:(NSString *)containerName withCompletion:(CompletionBlock) completion {
+    
+    
+    NSString *queryString = [NSString stringWithFormat:@"container=%@", containerName];
+    
+    [self.blobsTable readWithQueryString:queryString completion:^(NSArray *results, NSInteger totalCount, NSError *error) {
+        
+        [self logErrorIfNotNil:error];
+        
+        self.blobs = [results mutableCopy];
+        
+        // Let the caller know that we finished
+        completion();
+    }];
+}
+
+- (void) deleteBlob:(NSString *)blobName fromContainer:(NSString *)containerName withCompletion:(CompletionBlock) completion {
+    NSDictionary *idItem = @{ @"id" :@0 };
+    NSDictionary *params = @{ @"containerName" : containerName, @"blobName" : blobName };
+    
+    [self.blobsTable delete:idItem parameters:params completion:^(NSNumber *itemId, NSError *error) {
+        [self logErrorIfNotNil:error];
+        
+        NSLog(@"Results: %@", itemId);
+        
+        // Let the caller know that we finished
+        completion();
+    }];
+}
 
 - (void) getSasUrlForNewBlob:(NSString *)blobName forContainer:(NSString *)containerName withCompletion:(CompletionWithSasBlock) completion {
     NSDictionary *item = @{  };
     NSDictionary *params = @{ @"containerName" : containerName, @"blobName" : blobName };
+    
     [self.blobsTable insert:item parameters:params completion:^(NSDictionary *item, NSError *error) {
         NSLog(@"Item: %@", item);
+        
         completion([item objectForKey:@"sasUrl"]);
-       /* Once we get that back, we can then post those bytes from the client which it does with a NSMutableURLRequest and NSURLConnection.*/
     }];
 }
 
