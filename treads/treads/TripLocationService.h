1b5782d16ae4cf71e3f7cee1d07ec8daf9f9fa99
KanoffZA1 <KanoffZA1.gcc.edu>
2013-04-06 11:38:26 -0400

    Added TripLocationService

---
+++ treads/treads/TripLocationService.h
@@ -0,0 +1,13 @@
+//
+//  TripLocationService.h
+//  treads
+//
+//  Created by Zachary Kanoff on 4/6/13.
+//  Copyright (c) 2013 Team Walking Stick. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TripLocationService : NSObject
+
+@end
---
+++ treads/treads/TripLocationService.m
@@ -0,0 +1,13 @@
+//
+//  TripLocationService.m
+//  treads
+//
+//  Created by Zachary Kanoff on 4/6/13.
+//  Copyright (c) 2013 Team Walking Stick. All rights reserved.
+//
+
+#import "TripLocationService.h"
+
+@implementation TripLocationService
+
+@end
