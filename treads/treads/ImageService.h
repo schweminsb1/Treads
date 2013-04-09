58118ccb283c5e184c4df2af8b678fade08284a0
sam schwemin <schweminsb1@gcc.edu>
2013-04-09 14:38:13 -0400

    imageservice files added

---
+++ treads/treads/ImageService.h
@@ -0,0 +1,13 @@
+//
+//  ImageService.h
+//  treads
+//
+//  Created by Sam Schwemin on 4/9/13.
+//  Copyright (c) 2013 Team Walking Stick. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImageService : NSObject
+
+@end
---
+++ treads/treads/ImageService.m
@@ -0,0 +1,13 @@
+//
+//  ImageService.m
+//  treads
+//
+//  Created by Sam Schwemin on 4/9/13.
+//  Copyright (c) 2013 Team Walking Stick. All rights reserved.
+//
+
+#import "ImageService.h"
+
+@implementation ImageService
+
+@end
