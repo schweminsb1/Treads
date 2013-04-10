57d8eae30aadc99948c43dd44762df65ce26e85e
sam schwemin <schweminsb1@gcc.edu>
2013-04-10 13:50:26 -0400

    ImageService

--- treads/treads/ImageService.m
+++ treads/treads/ImageService.m
@@ -39,36 +39,14 @@
     return image;
 }
 
--(void) insertImageAsBlob:(UIImage *) image withCompletion:(MSItemBlock) ultimatecompletionblock
+-(void) insertImage:(UIImage *) image withCompletion:(MSItemBlock) ultimatecompletionblock
 {
-    CompletionWithSasBlock comp= ^(NSString* sas){
-        
-        _SASURL=sas;
-        NSData * data= UIImagePNGRepresentation(image);
-        NSMutableURLRequest * request= [NSMutableURLRequest requestWithURL:[NSURL URLWithString:_SASURL]];
-        [request setHTTPMethod:@"POST"];
-        [request setValue:[NSString stringWithFormat:@"%d", data.length] forHTTPHeaderField:@"Content-Length"];
-        [request setValue:@"application/x-www-form-urlencoded charset=utf-8" forHTTPHeaderField:@"Content-Type"];
-        [request setHTTPBody:data];
-        NSURLConnection *connection = [[NSURLConnection alloc] initWithRequest:request
-                                                                      delegate:self];
-        [connection start];
-        
-        //write to the url the UIIMageData
-       
-    };
     
-    CompletionWithItems complwithitems= ^(NSArray * items)
-    {
-        _imagesSizeNextImageID=items.count+5;
-     [_dataRepository getSasUrlForNewBlob:[NSString stringWithFormat:@"%d",_imagesSizeNextImageID] forContainer:@"images" withCompletion:comp];
-        
-        _comp=ultimatecompletionblock;
-    };
-   
-        [_dataRepository retrieveDataItemsMatching:@"id > -1" usingService:self withReturnBlock:complwithitems ];
-        
-        
+    NSMutableDictionary * imageDict= [[NSMutableDictionary alloc]init];
+    NSString * stringToSend= [self stringFromImage:image];
+    [imageDict setValue:stringToSend forKey:@"imageString"];
+    
+    [_dataRepository createDataItem:imageDict usingService:self withReturnBlock:ultimatecompletionblock];
         //insert image path into database for a newID
 
 
@@ -76,31 +54,19 @@
 }
 -(void) getImageWithPhotoID:(int) photoid withReturnBlock:(CompletionWithItems) comp
 {
-    CompletionWithSasBlock completion= ^(NSString* sas){
-        
-        _SASURL=sas;
-        NSData *data=[NSData dataWithContentsOfURL:[NSURL URLWithString:_SASURL]];
-  
-      //  UIImage * image= [UIImage imageWithData:data];
-       // NSArray * results= @[image];
-       // comp(results);
-
-        
-        //write to the url the UIIMageData
-        
-    };
-
-    [_dataRepository getSasUrlForNewBlob:[NSString stringWithFormat:@"%d",photoid] forContainer:@"images" withCompletion:completion];
 
     
+    [_dataRepository retrieveDataItemsMatching:[NSString stringWithFormat:@"id = %d",photoid] usingService:self withReturnBlock:comp];
 }
 
 
 - (NSArray*)convertReturnDataToServiceModel:(NSArray*)returnData
-
 {
-    
-    return returnData;
+    NSString * imagestring= returnData[0][@"imageString"];
+    UIImage * returnImage= [self imageFromString:imagestring];
+    NSArray * arr= @[returnImage];
+
+    return arr;
 }
 -(void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
 //post to the database the newdata item
