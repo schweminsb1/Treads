fba73c28ae9d2f3fcec9fd0b3fd0fb63c2dea0c2
sam schwemin <schweminsb1@gcc.edu>
2013-04-10 19:32:35 -0400

    LocationSearch is functioning

--- treads/treads/LocationService.h
+++ treads/treads/LocationService.h
@@ -5,26 +5,29 @@
 //  Created by Anthony DeLeone on 3/13/13.
 //  Copyright (c) 2013 Team Walking Stick. All rights reserved.
 //
-
 #import <Foundation/Foundation.h>
-
+#import "TreadsService.h"
 
 @class DataRepository;
 
-//typedef void (^MSReadQueryBlock)(NSArray *items,
-//NSInteger totalCount,
-//NSError *error);
 
-@interface LocationService : NSObject
+@interface LocationService : NSObject<TreadsService>
 
-@property DataRepository* dataRepository;
+
+@property (strong) DataRepository* dataRepository;
+@property (copy) NSString* dataTableIdentifier;
 
 - (id)initWithRepository:(DataRepository*)repository;
+- (NSArray*)convertReturnDataToServiceModel:(NSArray*)returnData;
+
+
 
 - (void)addLocation:(NSDictionary*)newLocation forTarget:(NSObject*) target withAction: (SEL) returnAction;
 
 - (void) getLocationsOrdered: (MSReadQueryBlock) getAll;
 
+- (void)getLocationByID:(int)LocationID forTarget:(NSObject *)target withAction:(SEL)returnAction;
 
+- (void)getLocationsforTarget:(NSObject *)target withAction:(SEL)returnAction;
 
 @end
--- treads/treads/LocationService.m
+++ treads/treads/LocationService.m
@@ -6,9 +6,11 @@
 //  Copyright (c) 2013 Team Walking Stick. All rights reserved.
 //
 
+
 #import "LocationService.h"
 #import "DataRepository.h"
 #import "Comment.h"
+#import "Location.h"
 @implementation LocationService
 
 
@@ -16,6 +18,7 @@
 {
     if(self = [super init])
     {
+        _dataTableIdentifier=@"LocationTable";
         _dataRepository = repository;
     }
     return self;
@@ -30,6 +33,47 @@
     [_dataRepository getLocationsOrdered:getAll];
 }
 
+- (NSArray*)convertReturnDataToServiceModel:(NSArray*)returnData
+{
+    NSMutableArray * results = [[NSMutableArray alloc]init];
+    for ( int i=0; i< returnData.count; i++)
+    {
+        Location * location  = [[Location alloc] init];
+        location.title  = returnData[i][@"name"];
+        location.idField  = returnData[i][@"LocationID"];
+        location.description = returnData[i][@"description"];
+        location.latitude     = [returnData[i][@"latitude"] floatValue];
+        location.longitude     = [returnData[i][@"longitude"] floatValue];
+        
+        //check to see if this user is saved on disk
+        //if he is, then load from disk
+        //if he is not, then send a request
+        //here is the request part
+        
+        //save this user to disk
+        // _userService getUserbyID:comment.UserID forTarget:self withAction:
+        //     NSArray * users;
+        //    int idval= [comment.UserID intValue];
+        // users=[_userService getUserbyID:idval withReturnItems:users];
+        // comment.commentsUser=users[0];
+        
+        [results addObject: location];
+        
+    }
+    return results;
+}
 
+- (void)getLocationByID:(int)LocationID forTarget:(NSObject *)target withAction:(SEL)returnAction
+{
+    [self.dataRepository retrieveDataItemsMatching:[NSString stringWithFormat:@"LocationID = '%d'", LocationID] usingService:self forRequestingObject:target withReturnAction:returnAction];
+    
+    
+}
+- (void)getLocationsforTarget:(NSObject *)target withAction:(SEL)returnAction
+{
+    [self.dataRepository retrieveDataItemsMatching:[NSString stringWithFormat:@"LocationID > %d", -1] usingService:self forRequestingObject:target withReturnAction:returnAction];
+    
+    
+}
 
 @end
