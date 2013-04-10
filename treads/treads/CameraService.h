8917c719046d647a797a1c5f6db14c597bae063a
DeLeoneAM1 <DeLeoneAM1@gcc.edu>
2013-04-10 14:56:23 -0400

    Added Camera Services files that are a work in progress.

---
+++ treads/treads/CameraService.h
@@ -0,0 +1,20 @@
+////
+////  CameraService.h
+////  treads
+////
+////  Created by Anthony DeLeone on 4/10/13.
+////  Copyright (c) 2013 Team Walking Stick. All rights reserved.
+////
+//
+//#import <UIKit/UIKit.h>
+//#import <MobileCoreServices/MobileCoreServices.h>
+//
+//
+//@interface CameraService : UIImagePickerController
+//
+//
+////- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker;
+////- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info;
+////- (void)image:(UIImage *)image finishedSavingWithError:(NSError *)error contextInfo:(void *)contextInfo;
+//- (void) showImagePicker: (UIImagePickerControllerSourceType *) sourceType;
+//@end
