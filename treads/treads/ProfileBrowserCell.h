c1a4d8453e494146591180e6f80941813604eac2
Ryan Keavney <keavneyrj1@gcc.edu>
2013-04-14 18:04:57 -0400

    profile browser cell

---
+++ treads/treads/ProfileBrowserCell.h
@@ -0,0 +1,13 @@
+//
+//  ProfileBrowserCell.h
+//  treads
+//
+//  Created by keavneyrj1 on 4/14/13.
+//  Copyright (c) 2013 Team Walking Stick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileBrowserCell : UITableViewCell
+
+@end
---
+++ treads/treads/ProfileBrowserCell.m
@@ -0,0 +1,29 @@
+//
+//  ProfileBrowserCell.m
+//  treads
+//
+//  Created by keavneyrj1 on 4/14/13.
+//  Copyright (c) 2013 Team Walking Stick. All rights reserved.
+//
+
+#import "ProfileBrowserCell.h"
+
+@implementation ProfileBrowserCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
