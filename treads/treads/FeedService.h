154cdec4c19e55c62f197d72b9b0f1b3c59ff7dc
Ryan Keavney <keavneyrj1@gcc.edu>
2013-04-14 23:52:34 -0400

    added feed service

---
+++ treads/treads/FeedService.h
@@ -0,0 +1,13 @@
+//
+//  FeedService.h
+//  treads
+//
+//  Created by keavneyrj1 on 4/14/13.
+//  Copyright (c) 2013 Team Walking Stick. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FeedService : NSObject
+
+@end
