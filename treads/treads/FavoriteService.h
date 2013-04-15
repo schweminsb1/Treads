9e7b07ebe6f7e96dcd0a094e6009c5c13da93ced
KanoffZA1 <KanoffZA1.gcc.edu>
2013-04-15 00:57:20 -0400

    Favoriting Implemented

--- treads/treads/FavoriteService.h
+++ treads/treads/FavoriteService.h
@@ -7,7 +7,23 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "TreadsService.h"
 
-@interface FavoriteService : NSObject
 
+@class DataRepository;
+
+@interface FavoriteService : NSObject <TreadsService>
+
+@property (strong) DataRepository* dataRepository;
+@property (copy) NSString* dataTableIdentifier;
+
+- (id)initWithRepository:(DataRepository*)repository;
+- (NSArray*)convertReturnDataToServiceModel:(NSArray*)returnData;
+
+- (void) getMyFavs:(int)myID forTarget:(NSObject*)target withAction:(SEL)returnAction;
+- (void) addFav:(int)myID withTripID:(int)tripID fromTarget:(NSObject *) target withReturn:(SEL) returnAction;
+- (void) deleteFav:(NSString*)fav fromTarget:(NSObject *) target withReturn:(SEL) returnAction;
+
++(FavoriteService*) instance ;
 @end
